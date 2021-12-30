01fadffdb1178e03af3f3d536e66dd2cd2bdabb5
darby <cutehackers@gmail.com>
2021-12-30 16:21:37 +0900

    added notification builder extension for fragment

--- notifire/src/main/java/app/darby/notifire/creator/FragmentNotifireCreator.kt
+++ notifire/src/main/java/app/darby/notifire/creator/FragmentNotifireCreator.kt
@@ -2,21 +2,103 @@
 
 package app.darby.notifire.creator
 
+import android.content.Context
+import androidx.core.app.Person
 import androidx.fragment.app.Fragment
-import app.darby.notifire.Notifire
+import app.darby.notifire.provider.BigPictureStyleBuilderProvider
+import app.darby.notifire.provider.BigTextStyleBuilderProvider
+import app.darby.notifire.provider.InboxStyleBuilderProvider
+import app.darby.notifire.provider.MessagingStyleBuilderProvider
 import app.darby.notifire.provider.NotifireBuilderProvider
 
 fun Fragment.notification(
     smallIconResId: Int? = null,
     channelId: String? = null,
     block: NotifireBuilderProvider
-): Notifire? {
+) = withApplicationContext {
+    notification(
+        applicationContext = it,
+        smallIconResId,
+        channelId,
+        block
+    ).notify()
+}
+
+fun Fragment.notificationAsBigTextStyle(
+    smallIconResId: Int? = null,
+    channelId: String? = null,
+    block: BigTextStyleBuilderProvider
+) = withApplicationContext {
+    notificationAsBigTextStyle(
+        applicationContext = it,
+        smallIconResId,
+        channelId,
+        block
+    ).notify()
+}
+
+fun Fragment.notificationAsBigPictureStyle(
+    smallIconResId: Int? = null,
+    channelId: String? = null,
+    block: BigPictureStyleBuilderProvider
+) = withApplicationContext {
+    notificationAsBigPictureStyle(
+        applicationContext = it,
+        smallIconResId,
+        channelId,
+        block
+    ).notify()
+}
+
+fun Fragment.notificationAsInboxStyle(
+    smallIconResId: Int? = null,
+    channelId: String? = null,
+    block: InboxStyleBuilderProvider
+) = withApplicationContext {
+    notificationAsInboxStyle(
+        applicationContext = it,
+        smallIconResId,
+        channelId,
+        block
+    ).notify()
+}
+
+
+@Deprecated(message = "deprecated, use notificationAsMessagingStyle with Person",
+    ReplaceWith("notificationAsMessagingStyle with Person argument")
+)
+fun Fragment.notificationAsMessagingStyle(
+    smallIconResId: Int? = null,
+    channelId: String? = null,
+    userDisplayName: CharSequence,
+    block: MessagingStyleBuilderProvider
+) = withApplicationContext {
+    notificationAsMessagingStyle(
+        applicationContext = it,
+        smallIconResId,
+        channelId,
+        userDisplayName,
+        block
+    ).notify()
+}
+
+fun Fragment.notificationAsMessagingStyle(
+    smallIconResId: Int? = null,
+    channelId: String? = null,
+    user: Person,
+    block: MessagingStyleBuilderProvider
+) = withApplicationContext {
+    notificationAsMessagingStyle(
+        applicationContext = it,
+        smallIconResId,
+        channelId,
+        user,
+        block
+    ).notify()
+}
+
+private fun <T> Fragment.withApplicationContext(block: (Context) -> T): T? {
     return activity?.applicationContext?.let {
-        notification(
-            applicationContext = it,
-            smallIconResId = smallIconResId,
-            channelId = channelId,
-            block
-        ).notify()
+        block(it)
     }
 }
