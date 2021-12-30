ab2513a74f56d06454c176b502afdd795dca4d61
darby <cutehackers@gmail.com>
2021-12-30 20:43:20 +0900

    added notification builder extension for service

--- notifire/src/main/java/app/darby/notifire/creator/ServiceNotifireCreator.kt
+++ notifire/src/main/java/app/darby/notifire/creator/ServiceNotifireCreator.kt
@@ -3,7 +3,12 @@
 package app.darby.notifire.creator
 
 import android.app.Service
+import androidx.core.app.Person
 import app.darby.notifire.Notifire
+import app.darby.notifire.provider.BigPictureStyleBuilderProvider
+import app.darby.notifire.provider.BigTextStyleBuilderProvider
+import app.darby.notifire.provider.InboxStyleBuilderProvider
+import app.darby.notifire.provider.MessagingStyleBuilderProvider
 import app.darby.notifire.provider.NotifireBuilderProvider
 
 fun Service.notification(
@@ -18,3 +23,65 @@ fun Service.notification(
         block
     ).notify()
 }
+
+fun Service.notificationAsBigTextStyle(
+    smallIconResId: Int? = null,
+    channelId: String? = null,
+    block: BigTextStyleBuilderProvider
+) = notificationAsBigTextStyle(
+    applicationContext,
+    smallIconResId,
+    channelId,
+    block
+).notify()
+
+fun Service.notificationAsBigPictureStyle(
+    smallIconResId: Int? = null,
+    channelId: String? = null,
+    block: BigPictureStyleBuilderProvider
+) = notificationAsBigPictureStyle(
+    applicationContext,
+    smallIconResId,
+    channelId,
+    block as NotifireBuilderProvider
+).notify()
+
+fun Service.notificationAsInboxStyle(
+    smallIconResId: Int? = null,
+    channelId: String? = null,
+    block: InboxStyleBuilderProvider
+) = notificationAsInboxStyle(
+    applicationContext,
+    smallIconResId,
+    channelId,
+    block as NotifireBuilderProvider
+).notify()
+
+@Deprecated(message = "deprecated, use notificationAsMessagingStyle with Person",
+    ReplaceWith("notificationAsMessagingStyle with Person argument")
+)
+fun Service.notificationAsMessagingStyle(
+    smallIconResId: Int? = null,
+    channelId: String? = null,
+    userDisplayName: CharSequence,
+    block: MessagingStyleBuilderProvider
+) = notificationAsMessagingStyle(
+    applicationContext,
+    smallIconResId,
+    channelId,
+    userDisplayName,
+    block as NotifireBuilderProvider
+).notify()
+
+fun Service.notificationAsMessagingStyle(
+    smallIconResId: Int? = null,
+    channelId: String? = null,
+    user: Person,
+    block: MessagingStyleBuilderProvider
+) = notificationAsMessagingStyle(
+    applicationContext,
+    smallIconResId,
+    channelId,
+    user,
+    block as NotifireBuilderProvider
+).notify()
