0efc6be81c77be0f127ebad9686129be57ed7c28
Darby <cutehackers@gmail.com>
2022-03-09 14:02:53 +0900

    added notifyAsBigPictureStyle() to MainActivity

--- sample/src/main/java/app/darby/sample/MainActivity.kt
+++ sample/src/main/java/app/darby/sample/MainActivity.kt
@@ -3,6 +3,7 @@ package app.darby.sample
 import android.app.PendingIntent
 import android.content.Intent
 import android.graphics.BitmapFactory
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -13,13 +14,16 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import androidx.core.app.RemoteInput
+import androidx.core.app.TaskStackBuilder
 import androidx.core.content.ContextCompat
 import app.darby.notifire.Notifire
-import app.darby.notifire.creator.extractMessagingStyleBuilderFromNotifire
+import app.darby.notifire.creator.newActionBuilder
+import app.darby.notifire.creator.notificationAsBigPictureStyle
 import app.darby.notifire.creator.notificationAsBigTextStyle
-import app.darby.notifire.creator.notificationAsMessagingStyle
-import app.darby.notifire.style.MessagingStyleBuilder
 import app.darby.sample.data.MockDatabase
+import app.darby.sample.handler.BigPictureSocialIntentService
+import app.darby.sample.handler.BigPictureSocialMainActivity
 import app.darby.sample.handler.BigTextIntentService
 import app.darby.sample.handler.BigTextMainActivity
 import com.google.android.material.snackbar.Snackbar
@@ -71,8 +75,7 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
         if (!notificationMgr.areNotificationsEnabled()) {
             Snackbar
                 .make(
-                    viewContainer,
-                    "You need to enable notifications for this app",
+                    viewContainer, "You need to enable notifications for this app",
                     Snackbar.LENGTH_LONG
                 )
                 .setAction("ENABLE") {
@@ -85,8 +88,8 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
 
         when (NOTIFICATION_STYLES[currentStylePosition]) {
             BIG_TEXT_STYLE -> notifyAsBigTextStyle()
-            //BIG_PICTURE_STYLE -> newBigPictureStyleNotification()
-            //INBOX_STYLE -> newInboxStyleNotification()
+            BIG_PICTURE_STYLE -> notifyAsBigPictureStyle()
+            INBOX_STYLE -> notifyAsInboxStyle()
             //MESSAGING_STYLE -> newMessagingStyleNotification()
             else -> {}
         }
@@ -188,6 +191,108 @@ class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
             // it several seconds later.
             NotifireBuilderCache.builder = this
         }
+    }
+
+    /*
+     * Generates a BIG_PICTURE_STYLE Notification that supports both phone/tablet and wear. For
+     * devices on API level 16 (4.1.x - Jelly Bean) and after, displays BIG_PICTURE_STYLE.
+     * Otherwise, displays a basic notification.
+     *
+     * This example Notification is a social post. It allows updating the notification with
+     * comments/responses via RemoteInput and the BigPictureSocialIntentService on 24+ (N+) and
+     * Wear devices.
+     */
+    private fun notifyAsBigPictureStyle() {
+        Log.d(TAG, "notifying BigPictureStyle notification message")
+
+        val bigPictureStyleSocialAppData = MockDatabase.getBigPictureStyleData()
+
+        // Set up main Intent for notification
+        val mainIntent = Intent(this, BigPictureSocialMainActivity::class.java)
+
+        TaskStackBuilder.create(this)
+            .addParentStack(BigPictureSocialMainActivity::class.java)
+            .addNextIntent(mainIntent)
+
+        val mainPendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            mainIntent,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+
+        // Set up RemoteInput, so users can input (keyboard and voice) from notification
+        val replyLabel = getString(R.string.reply_label)
+        val remoteInput = RemoteInput.Builder(BigPictureSocialIntentService.EXTRA_COMMENT)
+            .setLabel(replyLabel)
+            // List of quick response choices for any wearables paired with the phone
+            .setChoices(bigPictureStyleSocialAppData.possiblePostResponses)
+            .build()
+
+        val replyActionPendingIntent: PendingIntent =
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                val intent = Intent(this, BigPictureSocialIntentService::class.java)
+                    .apply {
+                        action = BigPictureSocialIntentService.ACTION_COMMENT
+                    }
+                PendingIntent.getService(this, 0, intent, 0)
+            } else {
+                mainPendingIntent
+            }
+
+        // Create a BigPictureStyle notification with Notifire extension
+        notificationAsBigPictureStyle(NOTIFICATION_ID) {
+            // BigPictureStyle
+            bigPicture(
+                BitmapFactory.decodeResource(
+                    resources,
+                    bigPictureStyleSocialAppData.bigImage
+                )
+            )
+            bigContentTitle(bigPictureStyleSocialAppData.bigContentTitle)
+            summaryText(bigPictureStyleSocialAppData.summaryText)
+            // General
+            contentTitle(bigPictureStyleSocialAppData.contentTitle)
+            settingsText(bigPictureStyleSocialAppData.contentTitle)
+            largeIcon(
+                BitmapFactory.decodeResource(
+                    resources,
+                    R.drawable.ic_person_black_48dp
+                )
+            )
+            contentIntent(mainPendingIntent)
+            color(ContextCompat.getColor(applicationContext, R.color.design_default_color_primary))
+            subText(1.toString())
+            category(NotificationCompat.CATEGORY_SOCIAL)
+            priority(NotificationCompat.PRIORITY_HIGH)
+            visibility(NotificationCompat.VISIBILITY_PRIVATE)
+
+            addAction {
+                // Create action with action builder
+                newActionBuilder(
+                    R.drawable.ic_reply_white_18dp,
+                    replyLabel,
+                    replyActionPendingIntent
+                ).run {
+                    addRemoteInput(remoteInput)
+                    build()
+                }
+            }
+
+            // Because we want this to be a new notification (not updating a previous notification), we
+            // create a new Builder. Later, we use the same global builder to get back the notification
+            // we built here for a comment on the post.
+            NotifireBuilderCache.builder = this
+        }
+    }
+
+    /*
+     * Generates a INBOX_STYLE Notification that supports both phone/tablet and wear. For devices
+     * on API level 16 (4.1.x - Jelly Bean) and after, displays INBOX_STYLE. Otherwise, displays a
+     * basic notification.
+     */
+    private fun notifyAsInboxStyle() {
+        Log.d(TAG, "notifying InboxStyle notification message")
     }
 
     private fun notifyWithBuilder() {
